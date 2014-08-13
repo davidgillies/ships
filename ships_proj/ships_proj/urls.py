59a4d8581302c8c7cb575393703005ba217f49d8
David Gillies <captain.daveg@googlemail.com>
2014-08-13 17:14:28 +0100

    change to gitignore and set up urls.py in the app

--- ships_proj/ships_proj/urls.py
+++ ships_proj/ships_proj/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ships_proj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'ships.views.index', name='home'),
+    # url(r'^$', 'ships.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^ships/', include('ships.urls')),
 )
