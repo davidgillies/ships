b335a94d690183f96183ba32d64001f7ea1e79e0
davidgillies <david.gillies.rhu@gmail.com>
2019-02-08 12:13:05 +0000

    Changing urls to 1.11 version.

--- ships_proj/ships_proj/urls.py
+++ ships_proj/ships_proj/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'ships_proj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^$', 'ships.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('ships.urls')),
-)
+]
