d9da4c7a68803dcf91057d9f16a49d1111436ff7
davidgillies <david.gillies.rhu@gmail.com>
2019-02-08 12:29:08 +0000

    more template updates

--- ships_proj/ships_proj/settings.py
+++ ships_proj/ships_proj/settings.py
@@ -68,9 +68,12 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS': {
-
-        },
+        'OPTIONS': [
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
     },
 ]
 
